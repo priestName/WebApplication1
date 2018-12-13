09e9de034278ac4c952e9ec259b60298c117207d
XR <731097019@qq.com>
2018-12-13 18:34:11 +0800

    baidu

--- ConsoleApplication4/Class1.cs
+++ ConsoleApplication4/Class1.cs
@@ -1,52 +1,38 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace ConsoleApplication4
 {
-    class Class6
+    class Class1
     {
-        public void saa()
+        // unit对话接口
+        public string unit_utterance()
         {
-            unit_utterance();
-
-
-        }
-        public static string unit_utterance()
-        {
-            //24.14dee8b928c79aaa22c7dab96637b9f4.2592000.1547275788.282335-15147577
-            //JObject job = JObject.Parse(getAccessToken());
-            //string token = job["access_token"].ToString();
-            string token = "24.14dee8b928c79aaa22c7dab96637b9f4.2592000.1547275788.282335-15147577";
-            string host = "https://unit.gz.baidubce.com/rpc/2.0/unit/bot/chat?access_token=" + token;
+            JObject job = JObject.Parse(getAccessToken());
+            string token = job["access_token"].ToString();
+            string host = "https://aip.baidubce.com/rpc/2.0/unit/service/chat?access_token=" + token;
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
             request.Method = "post";
             request.ContentType = "application/json";
             request.KeepAlive = true;
-            //string str = "{\"log_id\":\"UNITTEST_20181213\",\"version\":\"2.0\",\"service_id\":\"S10550\",\"session_id\":\"\",\"bot_session\":\"\",\"request\":{\"query\":\"你好\",\"user_id\":\"UNIT_WEB_d704bbbc51c74dd3a561e5529036f30f\",\"bernard_level\":\"0\",\"client_session\":{\"client_results\":\"\",\"candidate_options\":[]}},\"dialog_state\":{\"contexts\":{\"SYS_REMEMBERED_SKILLS\":[\"21441\",\"21443\"]}}}"; // json格式
-            string str= "{\"bot_session\":\"\",\"log_id\":\"7758521\",\"request\":{\"bernard_level\":0,\"client_session\":\"{\\\"client_results\\\":\\\"\\\", \\\"candidate_options\\\":[]}\",\"query\":\"你好\",\"query_info\":{\"asr_candidates\":[],\"source\":\"KEYBOARD\",\"type\":\"TEXT\"},\"updates\":\"\",\"user_id\":\"88888\"},\"bot_id\":\"21441\",\"version\":\"2.0\"}"; // json格式 
+            string str = "{\"log_id\":\"UNITTEST_10000\",\"version\":\"2.0\",\"service_id\":\"S10550\",\"session_id\":\"\",\"request\":{\"query\":\"北京天气\",\"bernard_level\":0,\"user_id\":\"88888\",\"client_session\":\"{\\\"client_results\\\":\\\"\\\", \\\"candidate_options\\\":[]}\"},\"query_info\":{\"asr_candidates\":[],\"source\":\"KEYBOARD\",\"type\":\"TEXT\"},\"updates\":\"\",\"dialog_state\":{\"contexts\":{\"SYS_REMEMBERED_SKILLS\":[\"21441\"]}}}"; // json格式
             byte[] buffer = Encoding.UTF8.GetBytes(str);
             request.ContentLength = buffer.Length;
             request.GetRequestStream().Write(buffer, 0, buffer.Length);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
             string result = reader.ReadToEnd();
-            JObject JobResult = JObject.Parse(result);
-            var Result_list= JobResult["result"]["response"]["action_list"].Select(s=>s["say"].ToString()).ToList();
-            
             Console.WriteLine("对话接口返回:");
-            Console.WriteLine(Result_list[1]);
+            Console.WriteLine(result);
             return result;
         }
 
-        
-        public static String getAccessToken()
+        public String getAccessToken()
         {
             // 调用getAccessToken()获取的 access_token建议根据expires_in 时间 设置缓存
             // 百度云中开通对应服务应用的 API Key 建议开通应用的时候多选服务
@@ -64,6 +50,5 @@ namespace ConsoleApplication4
             String result = response.Content.ReadAsStringAsync().Result;
             return result;
         }
-
     }
 }
---
+++ WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Baidu.Aip.Ocr;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Ocr client;
+        public Form3()
+        {
+            InitializeComponent();
+
+            string APP_ID = "15147577";
+            string API_KEY = "XG2oDGFRG6VWCzLXta0qKxEI";
+            string SECRET_KEY = "E48m5rAKTLlD4ZmE9mGBqIwXi7OiEbZQ ";
+
+            client = new Ocr(API_KEY, SECRET_KEY);
+            client.Timeout = 60000;  // 修改超时时间
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += textBox1.Text+"/n";
+            
+        }
+        public static string unit_utterance()
+        {
+            string token = "#####调用鉴权接口获取的token#####";
+            string host = "https://aip.baidubce.com/rpc/2.0/unit/bot/chat?access_token=" + token;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(host);
+            request.Method = "post";
+            request.ContentType = "application/json";
+            request.KeepAlive = true;
+            string str = "{\"log_id\":\"UNITTEST_10000\",\"version\":\"2.0\",\"service_id\":\"S10000\",\"session_id\":\"\",\"request\":{\"query\":\"你好\",\"user_id\":\"88888\"},\"dialog_state\":{\"contexts\":{\"SYS_REMEMBERED_SKILLS\":[\"1057\"]}}}"; // json格式
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string result = reader.ReadToEnd();
+            Console.WriteLine("对话接口返回:");
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
